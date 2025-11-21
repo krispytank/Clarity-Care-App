e79e741941aacd60f5f3cced407e966571a359b9
krispytank <brianrosh92@gmail.com>
2025-11-21 13:34:35 +0300

    change server port

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: "",
+    port: 'https://clarity-care-app.vercel.app/',
   },
   plugins: [
     tailwindcss(),
